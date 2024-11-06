3bea76e6b86c03bec33d2dd8d6c84753f9be1428
Mosabahmed255 <Musabahmed255@gmail.com>
2024-11-06 23:48:45 +0200

    Implement the Context API to streamline data flow within the application.

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,6 @@ import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return(
     <UserContext.Provider value={userData}>
       <ProfilePage />
