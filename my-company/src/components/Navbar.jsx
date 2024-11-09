41517b9988f7af69ea5b3a93de5cad3502751a05
Mosabahmed255 <Musabahmed255@gmail.com>
2024-11-09 16:37:11 +0200

    Create a four-page company website using React.

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav  style={{padding:'10px', backgroundColor:'#f0f0f0'}}>
+        <nav  style={{  display: 'flex', justifyContent: 'center',  padding:'10px', backgroundColor:'#f0f0f0'}}>
             <Link to="./Home.jsx" style={{margin:'0 10px'}}>Home</Link>
             <Link to="./About.jsx" style={{margin:'0 10px'}}>About</Link>
             <Link to="./Services.jsx" style={{margin:'0 10px'}}>Services</Link>
