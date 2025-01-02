343f6d4b7409570f8a8fa77c373159d1c555f7f0
Mosabahmed255 <Musabahmed255@gmail.com>
2025-01-02 12:06:27 +0200

    Advanced Routing in React with React Router.

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth'; // Assuming useAuth is a custom hook
 
-const ProtectedRoute = ({ isAuthenticated, children }) => {
-  return isAuthenticated ? children : <Navigate to="/" />;
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useAuth(); // Custom hook to determine auth status
+
+    if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+    }
+
+    return children;
 };
 
 export default ProtectedRoute;
